c5052a6422f922b94172c4c2676204c1cfec72c4
SergioBerteli <sergio@univali.br>
2024-05-15 22:15:34 -0300

    geração de saidas

--- perceptron_1_camada/perceptron.py
+++ perceptron_1_camada/perceptron.py
@@ -1,4 +1,6 @@
-from numpy import zeros
+from random import shuffle
+from numpy import zeros, array, stack
+from datasets.cientista_compositor import cientista_e_compositores
 def funcao_ativacao(x) -> int:
     """
     Como função de ativação foi escolhida a função degrau
@@ -7,24 +9,62 @@ def funcao_ativacao(x) -> int:
 
 class Neuronio:
     def __init__(self, n_ligacoes: int, x_i: float = 0) -> None:
-        self.__x_i = x_i
-        self.__sinapses = zeros((n_ligacoes,), dtype=int)
+        self.__x_i = x_i # Define o valor do neurônio
+        self.__sinapses = zeros((n_ligacoes,), dtype=int) # define os pesos sinápticos que começarão em 0
     
     def setX_i(self, x_i: float) -> None:
         self.__x_i = x_i
 
     def getX_i(self) -> float:
         return self.__x_i 
+    
+    def getSinapses(self):
+        return self.__sinapses
         
 
 
 
 class RedeNeural:
     def __init__(self, i_neuronios, o_neuronios) -> None:
-        __camada_entrada = []
-        __camada_entrada.append(Neuronio(o_neuronios, 1)) # Bias
+        self.__camada_entrada = []
+        self.__camada_entrada.append(Neuronio(o_neuronios, 1)) # Bias
         for _ in range(i_neuronios):
-            __camada_entrada.append(Neuronio(o_neuronios)) #instanciando a camada de entrada
-        __camada_saida = []
+            self.__camada_entrada.append(Neuronio(o_neuronios)) #instanciando a camada de entrada
+        self.__camada_saida = []
         for _ in range(o_neuronios):
-            __camada_saida.append(Neuronio(0)) # instanciando a camada de saida
+            self.__camada_saida.append(Neuronio(0)) # instanciando a camada de saida
+        self.__n_ligacoes_entrada = o_neuronios
+    
+    def treinar_rede(self, erro_conj_max, dataset):
+        erro_conj_per = 100
+        erro_conj_array = []
+        while erro_conj_max < erro_conj_per:
+            shuffle(dataset)
+            for sample in dataset:
+                self.insere_entrada(sample[0])
+                self.gera_saida()
+    
+    def gera_saida(self):
+        entradas = self.getCamadaEntrada()
+        matriz_pesos =  stack([neuronio.getSinapses() for neuronio in entradas], axis=-1)
+        vetor_valores_n = array([neuronio.getX_i() for neuronio in self.getCamadaEntrada()])
+        saida = matriz_pesos @ vetor_valores_n
+        print(saida)
+        pass
+        
+
+    
+    def getCamadaEntrada(self):
+        return self.__camada_entrada
+    
+    def getNLigacoesEntrada(self):
+        return self.__n_ligacoes_entrada
+
+    def insere_entrada(self, data):
+        for index, neuronio in enumerate(self.getCamadaEntrada()):
+            if index != 0: # pulando o bias:
+                neuronio.setX_i(data[index - 1])
+
+if __name__ == "__main__":
+    RNA = RedeNeural(2, 1)
+    RNA.treinar_rede(0, cientista_e_compositores)
